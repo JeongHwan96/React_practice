13a7833dfab430bce0962b81e70886167396f77f
JeongHwan96 <kosy17@naver.com>
2022-02-11 00:44:11 +0900

    React Practice

---
+++ src/components/Headers.jsx
@@ -0,0 +1,16 @@
+export default function Headers() {
+  return (
+    <header>
+      <div className="Header__container">
+        <div className="title">Awesome Prototypes in Shop</div>
+        <div className="subtitle">
+          Check out what other designers have created using ProtoPie—download
+          these examples to learn exactly how they made their interactions.
+        </div>
+        <a href="https://www.protopie.io" target="_BLANK" rel="noreferrer">
+          <button className="btn__area">Try ProtoPie Yourself</button>
+        </a>
+      </div>
+    </header>
+  );
+}
---
+++ src/components/Orders.jsx
@@ -0,0 +1,8 @@
+export default function Orders() {
+  return (
+    <div className="empty">
+      <div className="title">You don't have any orders</div>
+      <div className="subtitle">Click on a + to add an order</div>
+    </div>
+  );
+}
